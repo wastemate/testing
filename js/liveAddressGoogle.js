a4b9c382526e5fcb1c180e2fa1ea56d9a331038f
Jed Dawson <jed@edisoncode.com>
2015-08-04 23:35:53 -0700

    Built WasteMate.js from commit 83ff721 on branch cont-dep

--- js/liveAddressGoogle.js
+++ js/liveAddressGoogle.js
@@ -11,17 +11,26 @@ var setupLiveAddressGoogle = function( viewModel ) {
       }
     };
 
+    // first input
     var input = document.getElementById( 'street_address' );
     var autocomplete = new google.maps.places.Autocomplete( input, options );
 
-    // Geocoder
+    google.maps.event.addListener( autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+      parseAddress( [ place ], function() {
+          viewModel.show("categries");
+      } );
+      // console.log( 'place', place );
+    } );
 
-    $( '#form_street_address' ).on( 'submit', function( event ) {
+    // second (lob) input
+    var input2 = document.getElementById( 'lob_address' );
+    var autocomplete2 = new google.maps.places.Autocomplete( input2, options );
 
-      event.preventDefault();
-
-      onAddressSubmitted( $( input ).val() );
-
+    google.maps.event.addListener( autocomplete2, 'place_changed', function() {
+      var place = autocomplete2.getPlace();
+      parseAddress( [ place ] );
+      // console.log( 'place', place );
     } );
 
     var onAddressSubmitted = function( address ) {
@@ -34,45 +43,7 @@ var setupLiveAddressGoogle = function( viewModel ) {
 
         if ( status == google.maps.GeocoderStatus.OK && results[0] != null ) {
 
-          console.log( results );
-
-          var parts = results[0].address_components;
-
-          var address;
-
-          try {
-
-            address = {
-              primaryNumber : parts[0] ? parts[0].long_name : null,
-              street : ( parts[0] && parts[1] ) ? ( parts[0].long_name + ' ' + parts[1].long_name ) : null,
-              city: parts[2] ? parts[2].long_name : null,
-              stateShort: parts[4] ? parts[4].short_name : null,
-              zip: parts[6] ? parts[6].long_name : null,
-              lat: results[0].geometry.location.lat() ||  null,
-              lon: results[0].geometry.location.lng() || null,
-              rdi: ''                                                          
-            };
-
-            // debugger;
-
-            if ( !address.street || !address.primaryNumber || !address.city || !address.stateShort || !address.zip || !address.lat || !address.lon ) {
-              // missing required info
-              
-              alert( 'Oops. Bad address. Please enter your full address.' );
-
-              return;
-            }
-
-            //TOOD: what's rdi?
-
-          } catch( e ) {
-            throw( e );
-            return;
-          }
-
-          onParseData( address );
-
-          console.log( address );
+          parseAddress( results );
 
         } else {
           console.log( 'Something went wrong :(', results, status );
@@ -81,7 +52,47 @@ var setupLiveAddressGoogle = function( viewModel ) {
       } );
     };
 
-  var onParseData = function( address ) {
+  var parseAddress = function( results, next ) {
+
+    var parts = results[0].address_components;
+
+    var address;
+
+    try {
+
+      address = {
+        primaryNumber : parts[0] ? parts[0].long_name : null,
+        street : ( parts[0] && parts[1] ) ? ( parts[0].long_name + ' ' + parts[1].long_name ) : null,
+        city: parts[2] ? parts[2].long_name : null,
+        stateShort: parts[4] ? parts[4].short_name : null,
+        zip: parts[6] ? parts[6].long_name : null,
+        lat: results[0].geometry.location.lat() ||  null,
+        lon: results[0].geometry.location.lng() || null,
+        rdi: ''                                                          
+      };
+
+      // debugger;
+
+      if ( !address.street || !address.primaryNumber || !address.city || !address.stateShort || !address.zip || !address.lat || !address.lon ) {
+        // missing required info
+        
+        alert( 'Oops. Bad address. Please enter your full address.' );
+
+        return;
+      }
+
+      //TOOD: what's rdi?
+
+    } catch( e ) {
+      throw( e );
+      return;
+    }
+
+    onParseData( address, next );
+
+  };
+
+  var onParseData = function( address, next ) {
 
     var resetValues = function() {
       viewModel.address('');
@@ -118,13 +129,28 @@ var setupLiveAddressGoogle = function( viewModel ) {
         //service days should be an array 
         var service = serviceDays[0];
         viewModel.serviceDay(service.dow);
-        viewModel.show("categries");
+
+        $( '#lob_address' ).css( 'border', '2px solid #007700' );
+        
+        if ( next ) {
+          next();
+          console.log( 'next' );
+          return;
+        } 
+
+
 
         if(_debug){ console.log(serviceDays); }
       }, function(err){
+
         if(_debug){console.log(err);}
+        
+        $( '#lob_address' ).css( 'border', '2px solid #FFCCCC' );
+
         resetValues();
+
         alert("Oh drats, we don't have your address configured for online sign up. Call our office to speak to a human. 238-2381");
+
       });
     });
 
